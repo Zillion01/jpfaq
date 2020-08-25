a325d7b9c4d8002bf717b58ed3da494fe1642e7e
Christian Fries <hello@christian-fries.ch>
2020-08-25 22:55:36 +0200

    [TASK] Add persistence configuration for TYPO3 10 (#87623)
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    \Jp\Jpfaq\Domain\Model\TtContent::class => [
+        'tableName' => 'tt_content',
+        'properties' => [
+            'contentType' => [
+                'fieldName' => 'CType'
+            ],
+        ],
+    ],
+];
